410318880ab98541ddcb7381513332d2ed33aca2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-28 08:56:14 +0100

    Programma "Fattoriale" ultimato.

---
+++ UnitTest_Fattoriale/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_Fattoriale")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_Fattoriale")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1961c351-706e-40f5-9632-69e5c0c8ecb3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest_Fattoriale/UnitTest_Fattoriale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_Fattoriale
+{
+    [TestClass]
+    public class UnitTest_Fattoriale
+    {
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(5)]
+        [DataRow(10)]
+        [DataRow(50)]
+        public void TestMethod1(long numero)
+        {
+            long fattoriale;
+
+            fattoriale = Fattoriale.Program1.Fattoriale(numero);
+
+            Console.WriteLine("{0}!={1}", numero, fattoriale);
+        }
+    }
+}
